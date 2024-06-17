e7fe6527ca25e05a849dc8f722a1684e9744a7ee
RogerRaspini <rogerraspini099@gmail.com>
2024-06-16 22:05:26 -0300

    Roger Criação Testes
    testCalculateGetDiscount 2 e 3

--- src/test/java/br/cesul/employee/EmployeeTest.java
+++ src/test/java/br/cesul/employee/EmployeeTest.java
@@ -29,4 +29,41 @@ public class EmployeeTest {
 
         assertEquals(2180.3, employee.calculateIncome());
     }
+    @Test
+    @DisplayName("testCalculateGetDiscount2")
+    void testCalculateGetDiscount2() {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        var employee = new Employee(
+                "João",
+                "12345",
+                "6789",
+                LocalDate.parse("20/04/1982", dtf),
+                Sex.MALE,
+                3500D,
+                "555",
+                "666");
+
+        assertEquals(2944.8, employee.calculateIncome());
+
+
+    }
+
+    @Test
+    @DisplayName("testCalculateGetDiscount3")
+    void testCalculateGetDiscount3() {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        var employee = new Employee(
+                "João",
+                "12345",
+                "6789",
+                LocalDate.parse("20/04/1982", dtf),
+                Sex.MALE,
+                4500D,
+                "555",
+                "666");
+
+        assertEquals(3628.63, employee.calculateIncome());
+    }
 }
